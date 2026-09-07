e3e660e56227b34cd2faf0a9519a9d95dbc94189
John Rufino Macasaet <macasaetjohn@gmail.com>
2026-09-08 00:43:45 +0900

    [RV64_Single] Refactor code: remove registered ports, fix indents

--- hardware/cores/rv64_single_cycle/rtl/rv64_controller_single_cycle.sv
+++ hardware/cores/rv64_single_cycle/rtl/rv64_controller_single_cycle.sv
@@ -10,50 +10,52 @@
 module rv64_controller_single_cycle
 import typedefs_pkg::*;
 (
-    //==============================
-    //  Control Inputs
-    //==============================
-    input   [OpcodeWidth-1:0]      opcode,
-    input   [Funct3Width-1:0]      funct3,
-    input   [Funct7Width-1:0]      funct7,
-    //==============================
-    //  Control Outputs
-    //==============================
-    output reg                     Branch,
-    output reg                     MemRead,
-    output reg [PcSrcWidth-1:0]    PCSrc,
-    output reg [RegWrSrcWidth-1:0] RegWrSrc,
-    output alu_op_e                ALUOp,
-    output reg                     ALUSrc,
-    output reg                     RegWrite,
-    //==============================
-    //  Processor Outputs
-    //==============================
-    output reg                     wr_en,
-    output reg [WmaskWidth-1:0]   wmask
+  //==============================
+  //  Control Inputs
+  //==============================
+  input   [OpcodeWidth-1:0]      opcode,
+  input   [Funct3Width-1:0]      funct3,
+  input   [Funct7Width-1:0]      funct7,
+  //==============================
+  //  Control Outputs
+  //==============================
+  output                         Branch,
+  output                         MemRead,
+  output [PcSrcWidth-1:0]        PCSrc,
+  output [RegWrSrcWidth-1:0]     RegWrSrc,
+  output alu_op_e                ALUOp,
+  output                         ALUSrc,
+  output                         RegWrite,
+  //==============================
+  //  Processor Outputs
+  //==============================
+  output                         wr_en,
+  output [WmaskWidth-1:0]        wmask
 );
 
   alu_op_e  alu_op_d;
 
   // Registers
-  // logic                      Branch;
-  // logic                      MemRead;
-  // logic [PcSrcWidth-1:0]     PCSrc;
-  // logic [RegWrSrcWidth-1:0]  RegWrSrc;
-  // alu_op_e                   ALUOp;
-  // logic                      ALUSrc;
-  // logic                      RegWrite;
+  logic                      branch_d;
+  logic                      memread_d;
+  logic [PcSrcWidth-1:0]     pcsrc_d;
+  logic [RegWrSrcWidth-1:0]  regwrsrc_d;
+  alu_op_e                   aluop_d;
+  logic                      alusrc_d;
+  logic                      regwrite_d;
+  logic                      wr_en_d;
+  logic [WmaskWidth-1:0]     wmask_d;
 
   always_comb begin
-    Branch      = 0;
-    MemRead     = 0;
-    PCSrc       = 0;
-    RegWrSrc    = 0;
+    branch_d      = 0;
+    memread_d     = 0;
+    pcsrc_d       = 0;
+    regwrsrc_d    = 0;
     alu_op_d    = OP_DEFAULT;
-    ALUSrc      = 0;
-    RegWrite    = 0;
-    wr_en       = 0;
-    wmask       = 8'hFF;
+    alusrc_d      = 0;
+    regwrite_d    = 0;
+    wr_en_d       = 0;
+    wmask_d       = 8'hFF;
       
     case(opcode)
       //===============================================================================
@@ -61,29 +63,29 @@ import typedefs_pkg::*;
       //===============================================================================
       opcode_load: begin
         if(funct3==funct3_ld) begin
-          RegWrite    = 1'b1;     // Write to Register File
-          RegWrSrc    = 2'd1;     // Choose rdata input to write to Register File
+          regwrite_d    = 1'b1;     // Write to Register File
+          regwrsrc_d    = 2'd1;     // Choose rdata input to write to Register File
           alu_op_d    = OP_ADD;   // Add op
-          ALUSrc      = 1'b1;     // Choose i-type immediate as ALU inB
+          alusrc_d      = 1'b1;     // Choose i-type immediate as ALU inB
         end
       end
       
       opcode_op_immw: begin
         if(funct3==0) begin
-          RegWrite    = 1'b1;     // Write to Register File
-          RegWrSrc    = 2'd0;     // Choose ALURes to write to Register File
+          regwrite_d    = 1'b1;     // Write to Register File
+          regwrsrc_d    = 2'd0;     // Choose ALURes to write to Register File
           alu_op_d    = OP_ADD;   // Add ALU OP
-          ALUSrc      = 1'b1;     // Choose Immediate as ALU inB
+          alusrc_d      = 1'b1;     // Choose Immediate as ALU inB
         end
       end
       
       opcode_jalr: begin
         if(funct3==0) begin
-          RegWrite    = 1'b1;     // Write to Register File
-          RegWrSrc    = 2'd2;     // Choose PC+4 to write to Register File
+          regwrite_d    = 1'b1;     // Write to Register File
+          regwrsrc_d    = 2'd2;     // Choose PC+4 to write to Register File
           alu_op_d    = OP_ADD;   // Add ALU OP
-          ALUSrc      = 1'b1;     // Choose Immediate as ALU inB
-          PCSrc       = 2'd2;     // Choose ALURes to write to PC
+          alusrc_d      = 1'b1;     // Choose Immediate as ALU inB
+          pcsrc_d       = 2'd2;     // Choose ALURes to write to PC
         end
       end
       
@@ -94,13 +96,13 @@ import typedefs_pkg::*;
       opcode_stype: begin
         case(funct3) 
           funct3_sd: begin
-            ALUSrc      = 1'b1;     // Choose Immediate as ALU inB
+            alusrc_d      = 1'b1;     // Choose Immediate as ALU inB
             alu_op_d    = OP_ADD;   // Add ALU OP
-            wr_en       = 1'b1;     // Enable write to Data Memory
+            wr_en_d       = 1'b1;     // Enable write to Data Memory
           end
 
           // funct3_sb: begin
-          //   ALUSrc      = 
+          //   alusrc_d      = 
           // end
         endcase
       end
@@ -109,69 +111,63 @@ import typedefs_pkg::*;
       // R-TYPE
       //===============================================================================
       opcode_rtype: begin  
-          RegWrite    = 1'b1;         // Write to Register File         
-          case(funct3) 
-              funct3_addsub: begin 
-                  if(funct7==funct7_add)    
-                      alu_op_d = OP_ADD;
-                  else if(funct7==funct7_sub)
-                      alu_op_d = OP_SUB;
-              end
-              
-              funct3_and: begin
-                      alu_op_d = OP_AND; 
-              end
-              
-              funct3_or: begin 
-                      alu_op_d = OP_OR; 
-              end
-              
-              funct3_xor: begin 
-                      alu_op_d = OP_XOR; 
-              end
-              
-              funct3_slt: begin 
-                      alu_op_d = OP_SLT; 
-              end
-          endcase
+        regwrite_d    = 1'b1;         // Write to Register File         
+        case(funct3) 
+          funct3_addsub: begin 
+            if(funct7==funct7_add) alu_op_d = OP_ADD;
+            else if(funct7==funct7_sub) alu_op_d = OP_SUB;
+          end
+          funct3_and: alu_op_d = OP_AND; 
+          funct3_or:  alu_op_d = OP_OR; 
+          funct3_xor: alu_op_d = OP_XOR; 
+          funct3_slt: alu_op_d = OP_SLT; 
+        endcase
       end
       
       //===============================================================================
       // B-TYPE
       //===============================================================================
       opcode_btype: begin
-          Branch = 1'b1;
-          if(funct3==funct3_beq) begin
-              alu_op_d = OP_XOR;
-          end
-          else if(funct3==funct3_bne) begin
-              alu_op_d = OP_ISEQ;
-          end
+        branch_d = 1'b1;
+        if(funct3==funct3_beq) begin
+          alu_op_d = OP_XOR;
+        end
+        else if(funct3==funct3_bne) begin
+          alu_op_d = OP_ISEQ;
+        end
       end
       
       //===============================================================================
       // J-TYPE
       //===============================================================================
       opcode_jal: begin
-          RegWrSrc = 2'd2;
-          RegWrite = 1'b1;
-          PCSrc    = 1'b1;
+        regwrsrc_d = 2'd2;
+        regwrite_d = 1'b1;
+        pcsrc_d    = 1'b1;
       end
       
       default: begin
-          Branch      = 0;
-          MemRead     = 0;
-          PCSrc       = 0;
-          RegWrSrc    = 0;
-          alu_op_d    = OP_DEFAULT;
-          ALUSrc      = 0;
-          RegWrite    = 0;
-          wr_en       = 0;
-          wmask       = 0;
+        branch_d      = 0;
+        memread_d     = 0;
+        pcsrc_d       = 0;
+        regwrsrc_d    = 0;
+        alu_op_d      = OP_DEFAULT;
+        alusrc_d      = 0;
+        regwrite_d    = 0;
+        wr_en_d       = 0;
+        wmask_d       = 0;
       end
     endcase
   end
 
-  assign ALUOp = alu_op_d;
+  assign Branch   = branch_d;
+  assign MemRead  = memread_d;
+  assign PCSrc    = pcsrc_d;
+  assign RegWrSrc = regwrsrc_d;
+  assign ALUOp    = alu_op_d;
+  assign ALUSrc   = alusrc_d;
+  assign RegWrite = regwrite_d;
+  assign wr_en    = wr_en_d;
+  assign wmask    = wmask_d;
 
 endmodule // rv64_controller_single_cycle
--- hardware/cores/rv64_single_cycle/rtl/rv64_core_single_cycle.sv
+++ hardware/cores/rv64_single_cycle/rtl/rv64_core_single_cycle.sv
@@ -27,10 +27,8 @@ import typedefs_pkg::*;
   output  [WmaskWidth-1:0]   wmask,
   input   [RdataWidth-1:0]   rdata
 );
-
-  //==============================
-  // Wire declarations
-  //==============================
+  // Data Mem Address register
+  logic [AddrWidth-1:0] addr_d;
   
   //  RegFile to ALU/MUX Wires
   wire    [RdataWidth-1:0]   rd1_to_inA;     // read data 1 to ALU IN A
@@ -118,11 +116,11 @@ import typedefs_pkg::*;
   //  MUX3 (ALURes/Rdata/PC+4 To RegFile)
   //==============================
   prim_mux3 u_mux3_regwr(
-      .in0(ALURes),
-      .in1(rdata),
-      .in2({ {PcWidth{adder0_res[PcWidth-1]}}, adder0_res}), // sign extend to 64 bits
-      .sel(RegWrSrc),
-      .out(mux3_to_RegFile)
+    .in0(ALURes),
+    .in1(rdata),
+    .in2({ {PcWidth{adder0_res[PcWidth-1]}}, adder0_res}), // sign extend to 64 bits
+    .sel(RegWrSrc),
+    .out(mux3_to_RegFile)
   );
   
   //==============================
@@ -133,9 +131,9 @@ import typedefs_pkg::*;
   assign const_four = 32'd4;
   
   prim_add I_ADD_ADDER0(
-      .A    (pc_out     ),
-      .B    (const_four ),
-      .Sum  (adder0_res )
+    .A    (pc_out     ),
+    .B    (const_four ),
+    .Sum  (adder0_res )
   );
   
   //==============================
@@ -147,9 +145,9 @@ import typedefs_pkg::*;
   assign branch_imm = { {19{inst[31]}} ,inst[31],inst[7],inst[30:25],inst[11:8],1'b0};
   
   prim_add I_ADD_ADDER1(
-      .A(pc_out),
-      .B(branch_imm),
-      .Sum(adder1_res)
+    .A(pc_out),
+    .B(branch_imm),
+    .Sum(adder1_res)
   );
   
   //==============================
@@ -158,10 +156,10 @@ import typedefs_pkg::*;
   assign branch_sel = zero & Branch;
   
   prim_mux #(PcWidth) u_mux_pc4_branch(
-      .in0(adder0_res), 
-      .in1(adder1_res), 
-      .sel(branch_sel), 
-      .out(branch_res)
+    .in0(adder0_res), 
+    .in1(adder1_res), 
+    .sel(branch_sel), 
+    .out(branch_res)
   );
     
   //==============================
@@ -173,29 +171,29 @@ import typedefs_pkg::*;
   assign JAL_imm = { {11{inst[31]}} ,inst[31],inst[19:12],inst[20],inst[30:21],1'b0} ;
   
   prim_add I_ADD_ADDER2(
-      .A(pc_out),
-      .B(JAL_imm),
-      .Sum(adder2_res)
+    .A(pc_out),
+    .B(JAL_imm),
+    .Sum(adder2_res)
   );     
   
   //==============================
   // MUX3 (To PC)
   //==============================
   prim_mux3 #(PcWidth) u_mux3_to_pc (
-      .in0(branch_res),
-      .in1(adder2_res),
-      .in2(ALURes[PcWidth-1:0]),
-      .sel(PCSrc),
-      .out(mux_to_PC)
+    .in0(branch_res),
+    .in1(adder2_res),
+    .in2(ALURes[PcWidth-1:0]),
+    .sel(PCSrc),
+    .out(mux_to_PC)
   );
   //==============================
   //  ProgramCounter
   //==============================
   rv64_pc_reg u_rv64_pc_reg(
-      .clk_i    (clk_i      ), 
-      .rst_ni   (rst_ni     ),
-      .next_i   (mux_to_PC  ), 
-      .current_o(pc_out     )
+    .clk_i    (clk_i      ), 
+    .rst_ni   (rst_ni     ),
+    .next_i   (mux_to_PC  ), 
+    .current_o(pc_out     )
   );
   
   //==============================
@@ -209,37 +207,36 @@ import typedefs_pkg::*;
   //  Control
   //==============================
   rv64_controller_single_cycle u_rv64_controller(
-      .opcode(opcode), 
-      .funct3(funct3), 
-      .funct7(funct7),
-      .Branch(Branch), 
-      .MemRead(MemRead),
-      .PCSrc(PCSrc), 
-      .RegWrSrc(RegWrSrc),
-      .ALUOp(ALUOp), 
-      .ALUSrc(ALUSrc), 
-      .RegWrite(RegWrite),
-      .wr_en(wr_en), 
-      .wmask(wmask)
+    .opcode(opcode), 
+    .funct3(funct3), 
+    .funct7(funct7),
+    .Branch(Branch), 
+    .MemRead(MemRead),
+    .PCSrc(PCSrc), 
+    .RegWrSrc(RegWrSrc),
+    .ALUOp(ALUOp), 
+    .ALUSrc(ALUSrc), 
+    .RegWrite(RegWrite),
+    .wr_en(wr_en), 
+    .wmask(wmask)
   );
+
+  //==============================
+  //  Data Mem Address
+  //==============================
+  always@(*) begin
+    if(!rst_ni) 
+      addr_d = 0;
+    else
+      addr_d = ALURes[AddrWidth-1:0];
+  end
   
   //==============================
   // Processor Outputs
   //==============================
-  
-  // To Instruction Memory
   assign pc = pc_out[PcWidth-1:0];
-  
-  // To Data Memory
-  always@(*) begin
-      if(!rst_ni) 
-          addr = 0;
-      else
-          addr = ALURes[AddrWidth-1:0];
-  end
-  //wr_en connected to control output
+  assign addr  = addr_d;
   assign wdata = rd2_out;
-  //wmask connected to control output
   
   
 
